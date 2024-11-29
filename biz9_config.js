e54ed414d0641f4a1583a6e2b01c9adc41bf08e4
think1 <think1@localhost.localdomain>
2024-11-29 16:46:49 -0500

    good

--- biz9_config.js
+++ biz9_config.js
@@ -1,4 +1,4 @@
-exports.VERSION="1.0.30"
+exports.VERSION="1.0.31"
 exports.TITLE='BiZ9-System-Unstable'
 exports.REPO='git@github.com:biz9framework/biz9-system-unstable.git'
 exports.BRANCH='1.1.3'
